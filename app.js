f1f2e5010e43ebec5eb86dd6a74611a4fc390274
Jim Mars <jimmars@Kratos-Slate.hsd1.or.comcast.net>
2020-12-15 23:46:35 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello n00bs!'));
+app.get('/', (req, res) => res.send('Major change!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
